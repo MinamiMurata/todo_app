093363ac3a0f2297901700bdb7bbbd6dc44bd9f9
minami murata <minami_murata@outlook.com>
2023-06-18 03:26:04 +0900

    add tasks create for seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# 50.times do |n|
-#   title = Faker::JapaneseMedia::DragonBall.character
-#   content = Faker::JapaneseMedia::DragonBall.planet
-#   deadline = 7.days.from_now
-#   Task.create!(title: title, content: content, deadline: deadline)
-# end
-
 10.times do |n|
   name = Faker::Games::SuperMario.name
   email = Faker::Internet.email
@@ -16,3 +9,12 @@ name_admin = "admin1"
 email_admin = "admin1@gmail.com"
 password_admin = "adminuser"
 User.create!(name: name_admin, email: email_admin, password: password_admin, admin: true)
+
+User.all.each do |user|
+  title = Faker::JapaneseMedia::DragonBall.character
+  content = Faker::JapaneseMedia::DragonBall.planet
+  deadline = 7.days.from_now
+  status = 1
+  priority = 1
+  user.tasks.create!(title: title, content: content, deadline: deadline, status: status, priority: priority)
+end
